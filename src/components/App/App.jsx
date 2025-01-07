f42327cb20e5a61e9cd97dc03bb5d701fa31116b
Tia Giron <tiagiron01@gmail.com>
2025-01-07 15:14:22 -0600

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hey gir</h1>
+      <p>This is your final project</p>
+    </>
+  );
+}
+
+export default App;
